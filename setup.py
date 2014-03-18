87f2f421b4996d217514739267bfa6a6f8d1daa3
Rahul Ranjan <rahul.rrixe@gmail.com>
2014-03-18 06:01:33 +0530

    initial commits

---
+++ setup.py
@@ -0,0 +1,128 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools import Command
+from setuptools.command.install import install as _install
+from subprocess import call
+from unittest import TextTestRunner, TestLoader
+from glob import glob
+from os.path import splitext, basename, join as pjoin
+from itertools import ifilterfalse
+
+TEST_PATHS = ['tests', ]
+
+SUPPORTED_VERSIONS = ['2.5', '2.6', '2.7', 'PyPy', ]
+
+if sys.version_info <= (2, 4):
+    version = '.'.join([str(x) for x in sys.version_info[:3]])
+    print('Version ' + version + ' is not supported. Supported versions are ' +
+          ', '.join(SUPPORTED_VERSIONS))
+    sys.exit(1)
+
+
+class TestCommand(Command):
+    description = "run test suite"
+    user_options = []
+
+    def initialize_options(self):
+        THIS_DIR = os.path.abspath(os.path.split(__file__)[0])
+        sys.path.insert(0, THIS_DIR)
+        for test_path in TEST_PATHS:
+            sys.path.insert(0, pjoin(THIS_DIR, test_path))
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        status = self._run_tests()
+        sys.exit(status)
+
+    def _run_tests(self):
+        pre_python26 = (sys.version_info[0] == 2
+                        and sys.version_info[1] < 6)
+        if pre_python26:
+            missing = []
+            # test for dependencies
+            try:
+                import simplejson
+                simplejson              # silence pyflakes
+            except ImportError:
+                missing.append("simplejson")
+
+            try:
+                import ssl
+                ssl                     # silence pyflakes
+            except ImportError:
+                missing.append("ssl")
+
+            if missing:
+                print("Missing dependencies: " + ", ".join(missing))
+                sys.exit(1)
+
+        testfiles = []
+        for test_path in TEST_PATHS:
+            for t in glob(pjoin(self._dir, test_path, 'test_*.py')):
+                testfiles.append('.'.join(
+                    [test_path.replace('/', '.'), splitext(basename(t))[0]]))
+
+        tests = TestLoader().loadTestsFromNames(testfiles)
+
+        #        for test_module in DOC_TEST_MODULES:
+        #            tests.addTests(doctest.DocTestSuite(test_module))
+
+        t = TextTestRunner(verbosity=2)
+        res = t.run(tests)
+        return not res.wasSuccessful()
+
+
+class Pep8Command(Command):
+    description = "run pep8 script"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            import pep8
+
+            pep8
+        except ImportError:
+            print ('Missing "pep8" library. You can install it using pip: '
+                   'pip install pep8')
+            sys.exit(1)
+
+        cwd = os.getcwd()
+        retcode = call(('pep8 %s/libcloud_rest/ %s/tests/' %
+                        (cwd, cwd)).split(' '))
+        sys.exit(retcode)
+
+setup(
+    name='parinx',
+    version='0.0.1',
+    packages=find_packages(),
+    package_dir={'parinx': 'parinx'},
+    install_requires=[
+        'unittest2',
+    ],
+    url='https://github.com/islamgulov/libcloud.rest/',
+    license='Apache License (2.0)',
+    author='ilgiz',
+    author_email='',
+    description='REST Interface for Libcloud',
+    entry_points='''
+            [console_scripts]
+            libcloud_rest = libcloud_rest.server:main
+            ''',
+    cmdclass={
+        'pep8': Pep8Command,
+        'test': TestCommand,
+        'install': install,
+    },
+)
---
+++ tests/test_parser.py
@@ -0,0 +1,205 @@
+# -*- coding:utf-8 -*-
+import unittest2
+
+from parinx import parser
+from parinx.errors import MethodParsingException
+
+
+class TestParser(unittest2.TestCase):
+
+    def test_get_docsring(self):
+        class A(object):
+            def foo(self):
+                """docstring"""
+
+            def bar(self):
+                pass
+
+        class B(A):
+            def foo(self):
+                pass
+
+            def bar(self):
+                pass
+
+        self.assertEqual(parser.get_method_docstring(B, 'foo'), 'docstring')
+        self.assertEqual(parser.get_method_docstring(B, 'bar'), None)
+
+    def test_parse_docstring(self):
+        docstring = """
+        Return a Zone instance.
+        Second line docsting.
+
+        :type zone_id: ``str``
+        :param zone_id: Required zone id (required)
+
+        :keyword    auth:   Initial authentication information for the node
+                            (optional)
+        :type       auth: :class:`NodeAuthSSHKey` or `NodeAuthPassword`
+
+        :return:    instance
+        :rtype: :class:`Zone` or `Node`
+        """
+        result = parser.parse_docstring(docstring)
+        description = result['description']
+        args = result['arguments']
+        return_type = result['return']['type_name']
+        return_description = result['return']['description']
+        self.assertTrue(description.startswith('Return'))
+        self.assertTrue('Second line' in description)
+        self.assertEqual(args['zone_id']['type_name'], '``str``')
+        self.assertEqual(args['zone_id']['required'], True)
+        self.assertItemsEqual(args['auth']['type_name'],
+                              'class:`NodeAuthSSHKey` or `NodeAuthPassword`')
+        self.assertEqual(args['auth']['required'], False)
+        self.assertEqual(return_type, 'class:`Zone` or `Node`')
+        self.assertEqual(return_description, 'instance')
+
+    def test_parse_docstring_fails(self):
+        docstring = """
+        Return a Zone instance.
+        Second line docsting.
+
+        :type zone_id: ``str``
+        :param zone_id: Required zone id (required)
+
+        :return: :class:`Zone` or `Node` instance.
+        """
+        self.assertRaises(MethodParsingException,
+                          parser.parse_docstring, docstring)
+        docstring = """
+        Return a Zone instance.
+        Second line docsting.
+
+        :type zone_id:  ``str``
+        :param zone_id: Required zone id (required)
+
+        :return: instance.
+        """
+        self.assertRaises(MethodParsingException,
+                          parser.parse_docstring, docstring)
+
+    def test_parse_args(self):
+        class A(object):
+            def update_zone(self, zone, domain, type='master',
+                            ttl=None, extra=None):
+                pass
+
+        result = parser.parse_args(A.update_zone)
+        self.assertEqual(result.keys(),
+                         ['zone', 'domain', 'type', 'ttl', 'extra'])
+        self.assertEqual(result['zone']['required'], True)
+        self.assertEqual(result['type']['required'], False)
+        self.assertFalse('default' in result['zone'])
+        self.assertTrue('default' in result['ttl'])
+
+
+class InheritParseDocstringTests(unittest2.TestCase):
+    class Foo(object):
+        def create_node(self, **kwargs):
+            """
+            Create a new node instance.
+
+            :keyword    name:   String with a name for this new node (required)
+            :type       name:   ``str``
+
+            :keyword    size:   The size of resources allocated to this node.
+                                (required)
+            :type       size:   ``dict``
+
+            :return: The newly created node.
+            :rtype: :class:`Node`
+            """
+
+        def deploy_node(self, **kwargs):
+            """
+            Deploy a new node, and start deployment.
+
+            Depends on a Provider Driver supporting either using a specific
+            password or returning a generated password.
+
+            @inherits: :class:`Foo.create_node`
+
+            :keyword    deploy: Deployment to run once machine is online and
+                                availble to SSH.
+            :type       deploy: :class:`NodeAuthSSHKey`
+            """
+
+    class Bar(Foo):
+        def create_node(self, **kwargs):
+            """
+            Create a new bar node
+
+            @inherits: :class:`Foo.create_node`
+
+            :keyword    ex_fqdn:   Fully Qualified domain of the node
+            :type       ex_fqdn:   ``str``
+
+            :return: New bar node
+            """
+
+        def deploy_node(self, **kwargs):
+            """
+            Deploy bar node
+
+            @inherits: :class:`Foo.deploy_node`
+            """
+
+    def test_foo_create_node(self):
+        docstring = parser.get_method_docstring(self.Foo, 'create_node')
+        result = parser.parse_docstring(docstring, self.Foo)
+        description = result['description']
+        args = result['arguments']
+        return_type = result['return']['type_name']
+        return_description = result['return']['description']
+        self.assertTrue(description.startswith('Create a new node '))
+        self.assertEqual(len(args), 2)
+        self.assertEqual('``str``', args['name']['type_name'])
+        self.assertEqual('``dict``', args['size']['type_name'])
+        self.assertEqual(return_type, 'class:`Node`')
+        self.assertEqual(return_description, 'The newly created node.')
+
+    def test_foo_deploy_node(self):
+        docstring = parser.get_method_docstring(self.Foo, 'deploy_node')
+        result = parser.parse_docstring(docstring, self.Foo)
+        description = result['description']
+        args = result['arguments']
+        return_type = result['return']['type_name']
+        return_description = result['return']['description']
+        self.assertTrue(description.startswith('Deploy a new node'))
+        self.assertEqual(len(args), 3)
+        self.assertEqual('class:`NodeAuthSSHKey`', args['deploy']['type_name'])
+        self.assertEqual('``str``', args['name']['type_name'])
+        self.assertEqual('``dict``', args['size']['type_name'])
+        self.assertEqual(return_type, 'class:`Node`')
+        self.assertEqual(return_description, 'The newly created node.')
+
+    def test_bar_create_node(self):
+        docstring = parser.get_method_docstring(self.Bar, 'create_node')
+        result = parser.parse_docstring(docstring, self.Bar)
+        description = result['description']
+        args = result['arguments']
+        return_type = result['return']['type_name']
+        return_description = result['return']['description']
+        self.assertTrue(description.startswith('Create a new bar node'))
+        self.assertEqual(len(args), 3)
+        self.assertEqual('``str``', args['name']['type_name'])
+        self.assertEqual('``dict``', args['size']['type_name'])
+        self.assertEqual('``str``', args['ex_fqdn']['type_name'])
+        self.assertEqual(return_type, 'class:`Node`')
+        self.assertEqual(return_description, 'New bar node')
+
+    def test_bar_deploy_node(self):
+        docstring = parser.get_method_docstring(self.Bar, 'deploy_node')
+        result = parser.parse_docstring(docstring, self.Bar)
+        description = result['description']
+        args = result['arguments']
+        return_type = result['return']['type_name']
+        return_description = result['return']['description']
+        self.assertTrue(description.startswith('Deploy bar node'))
+        self.assertEqual(len(args), 3)
+        self.assertEqual('class:`NodeAuthSSHKey`', args['deploy']['type_name'])
+        self.assertEqual('``str``', args['name']['type_name'])
+        self.assertEqual('``dict``', args['size']['type_name'])
+        self.assertEqual(return_type, 'class:`Node`')
+        self.assertEqual(return_description, 'The newly created node.')
